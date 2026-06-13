5a4214d7d5918f173f0e2712642d6b561120f710
Dave Redfern <dave-redfern@users.noreply.github.com>
2026-06-13 19:58:42 -0400

    Fix Manager not being tagged public reliably

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -6,21 +6,25 @@ use League\Fractal\Manager;
 use League\Fractal\ScopeFactoryInterface;
 use Somnambulist\Bundles\FractalBundle\ScopeFactory;
 
-return static function(ContainerConfigurator $container) {
+return static function (ContainerConfigurator $container) {
     $services = $container->services();
     $parameters = $container->parameters();
 
-    $services->defaults()
+    $services
+        ->defaults()
         ->private()
         ->autowire()
-        ->autoconfigure();
+        ->autoconfigure()
+    ;
 
-    $services->set(Manager::class, Manager::class)
-        ->alias('somnambulist.fractal_bundle.manager', Manager::class)
-        ->public();
+    $services->set(Manager::class, Manager::class)->public();
 
-    $services->set(ScopeFactory::class, ScopeFactory::class)
-        ->arg('$transformers', tagged_locator('somnambulist.fractal_bundle.transformer'));
+    $services->alias('somnambulist.fractal_bundle.manager', Manager::class)->public();
+
+    $services
+        ->set(ScopeFactory::class, ScopeFactory::class)
+        ->arg('$transformers', tagged_locator('somnambulist.fractal_bundle.transformer'))
+    ;
 
     $services->alias(ScopeFactoryInterface::class, ScopeFactory::class);
 };
